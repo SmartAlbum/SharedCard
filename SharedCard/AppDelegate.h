ad2e2f71f5329683310207db88237f9864988765
yongry <527068687@qq.com>
2015-11-25 02:05:26 +0800

    Initial Commit

---
+++ SharedCard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SharedCard
+//
+//  Created by JessieYong on 15/11/25.
+//  Copyright © 2015年 JessieYong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SharedCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SharedCard
+//
+//  Created by JessieYong on 15/11/25.
+//  Copyright © 2015年 JessieYong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
