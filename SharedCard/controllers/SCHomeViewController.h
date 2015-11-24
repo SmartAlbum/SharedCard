c4a5d907193982b5338ed684a99c2c5a9f6b5a77
yongry <527068687@qq.com>
2015-11-25 02:12:21 +0800

    frame

--- SharedCard/controllers/SCHomeViewController.h
+++ SharedCard/controllers/SCHomeViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  SCHomeViewController.h
 //  SharedCard
 //
 //  Created by JessieYong on 15/11/25.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface SCHomeViewController : UIViewController
 
 @end
-
