5c7bf6fa3ae77e8993286ef8b405e36bd5510474
Ring <226214208@qq.com>
2015-12-02 13:17:34 +0800

    resolve shuffle

--- SharedCard/models/Game.swift
+++ SharedCard/models/Game.swift
@@ -7,15 +7,29 @@
 //
 
 import Foundation
-extension Array {
-    mutating func shuffle() {
+extension CollectionType {
+    /// Return a copy of `self` with its elements shuffled
+    func shuffle() -> [Generator.Element] {
+        var list = Array(self)
+        list.shuffleInPlace()
+        return list
+    }
+}
+
+extension MutableCollectionType where Index == Int {
+    /// Shuffle the elements of `self` in-place.
+    mutating func shuffleInPlace() {
+        // empty and single-element collections don't shuffle
         if count < 2 { return }
-        for i in 0..<(count - 1) {
+        
+        for i in 0..<count - 1 {
             let j = Int(arc4random_uniform(UInt32(count - i))) + i
-//            swap(&self[i], &self[j])
+            guard i != j else { continue }
+            swap(&self[i], &self[j])
         }
     }
 }
+
 
 
 enum GameResult{
